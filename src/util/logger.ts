7bd90b3f345f165b9dedbfab87bcc7fe27ecb69e
Hiep Le <hieptuanle@gmail.com>
2019-02-11 01:53:22 +0700

    Upgrade stuff

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,17 +1,17 @@
-import winston from "winston";
-import { Logger } from "winston";
-import { ENVIRONMENT } from "./secrets";
+import winston from 'winston'
+import { ENVIRONMENT } from './secrets'
 
-const logger = new (Logger)({
-    transports: [
-        new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
-        new (winston.transports.File)({ filename: "debug.log", level: "debug"})
-    ]
-});
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+    }),
+    new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
+  ],
+})
 
-if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
+if (process.env.NODE_ENV !== 'production') {
+  logger.debug('Logging initialized at debug level')
 }
 
-export default logger;
-
+export default logger
